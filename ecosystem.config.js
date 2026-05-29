92c86f423b3f48799d80a7ee2feffb13fe74375d
Ugan Saripudin <ugan.saripudin@sprout.co.id>
2026-05-30 03:54:19 +0700

    add nvmrc and pm2 ecosystem config for vps deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'meridian',
+    script: 'index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    time: true,
+    merge_logs: true
+  }]
+};
